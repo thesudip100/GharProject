0ea578b615fb2357886cde92a76e10d023d40230
Sudip <sudippaudel944@gmail.com>
2023-10-01 14:38:56 +0545

    Final Commit except Validations

--- GharSewa/home/urls.py
+++ GharSewa/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.aboutPage, name='about'),
     path('customer_register/', views.customer_register, name='customer_register'),
     path('prof_register/', views.prof_register, name='prof_register'),
     path('login/', views.handleLogin, name='login'),
@@ -13,5 +14,9 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('services/<int:myid>/', views.serviceView, name='serviceView'),
     path('booking/<int:pid>', views.customerBooking, name='booking'),
-    path('payment/',views.payment,name='payment')
+    path('verify/', views.verify, name='verify'),
+    path('editprofile/', views.edit_profile, name='editprofile'),
+    path('bookingdetails/',views.booking_detail,name='bookingdetails'),
+    path('user_booking/', views.profBooking, name='user_booking'),
+    path('cancel/<int:cancel_id>/', views.cancelBooking, name='cancelBooking'),
     ]
--- GharSewa/home/views.py
+++ GharSewa/home/views.py
@@ -6,16 +6,21 @@ from django.contrib import messages
 from home.models import *
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+import requests
+import json
 
 from home.urls import *
-User=get_user_model()
+
 
 # Create your views here.
 def index(request):
+    print("You are :", request.session.get('username'))
+    print("You are :", request.session.get('user_id'))
     return render(request,'index.html')
 
-# def indexReturn(request):
-#      return redirect(index)
+def aboutPage(request):
+    return render(request,'about.html')
+
 
 def customer_register(request):
         user="none"
@@ -69,11 +74,9 @@ def prof_register(request):
                service = Service.objects.get(service_id=request.POST['service'])
                city = City.objects.get(id=request.POST['city'])
 
-               stat = Status.objects.get(status='pending')
               
-               profRegister.objects.create(firstName=firstName,lastName=lastName,email=email,fullAddress=fullAddress,phone = phone,profilePic = profilePic,experience = experience,training_certificate=training_certificate,user_id=user.id,service=service, city=city,status=stat)
-            #    professional_group=Group.objects.get(name='Professional')
-            #    professional_group.user_set.add(user)
+               profRegister.objects.create(firstName=firstName,lastName=lastName,email=email,fullAddress=fullAddress,phone = phone,profilePic = profilePic,experience = experience,training_certificate=training_certificate,user_id=user.id,service=service, city=city,)
+
                return HttpResponse("Professional registered successfully")
      
      context = {'services': services, 'city': city}
@@ -89,16 +92,10 @@ def handleLogin(request):
           user=authenticate(request,username=userName,password=passWord)
           
           if user is not None:
+                request.session['username']=user.username
+                request.session['user_id']=user.id
                 login(request, user)
                 return redirect(index)
-                # g=request.user.groups.all()[0].name
-                # if g=='Customer':
-                #     context=True
-                #     return render(request, 'index.html', {'context':context})
-                    
-                # elif g=='Professional':
-                #     context=False
-                #     return render(request,'index.html',{'context':context})
           
      return render(request, 'login.html')
 
@@ -122,6 +119,65 @@ def handleLogout(request):
     logout(request)
     return redirect('/.')
 
+@login_required(login_url='login')
+def edit_profile(request):
+    u = User.objects.get(id=request.user.id)
+    
+
+    try:
+        page = True
+        user = profRegister.objects.get(user=u)
+  
+        
+
+    except:
+        page = False
+        user = userRegister.objects.get(user=u)
+        
+
+    services = Service.objects.all()
+    city = City.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+
+        try:
+            image = request.FILES['image']
+            user.profilePic = image
+            user.save()
+        except:
+            pass
+
+        try:
+            user.experience = data['exp']
+            serv = Service.objects.get(service_id=data['service'])
+            ct = City.objects.get(id=data['city'])
+            user.service = serv
+            user.city = ct
+            certificate = request.FILES['cert']
+            user.training_certificate = certificate
+            user.save() 
+            
+        except:
+            pass
+        
+        user.fullAddress = data['address']
+        user.phone = data['phone']
+        u.username = data['username']
+        user.firstName = data['fname']
+        user.lastName = data['lname']
+        user.email = data['email']
+        u.first_name=data['fname']
+        u.last_name=data['lname']
+        u.email = data['email']
+
+        u.save()
+        user.save()
+        messages.success(request, 'Profile Updated!!')
+        return redirect('profile')
+
+    context = {'page': page, 'services': services, 'city': city, 'user': user}
+    return render(request,'editprofile.html',context)
 
 
 @login_required(login_url='login')
@@ -153,26 +209,82 @@ def customerBooking(request, pid):
         return redirect('services')
 
     prof = profRegister.objects.get(user=pid)
+    
  
 
     # book=Booking.objects.get(customer=client)
 
     if request.method == 'POST':
+        url = "https://a.khalti.com/api/v2/epayment/initiate/"
+
+        return_url = request.POST.get("return_url")
+        amount = request.POST.get("amount")
+        purchase_order_id = request.POST.get("purchase_order_id")
+
+        print("return_url::::::",return_url)
+
+
+        payload = json.dumps({
+            "return_url": return_url,
+            "website_url": "http://127.0.0.1:8000",
+            "amount": amount,
+            "purchase_order_id": purchase_order_id,
+            "purchase_order_name": "test",
+            "customer_info": {
+            "name": request.user.username,
+            "email": request.user.email,
+            "phone": "9800000001"
+            }
+        })
+        headers = {
+            'Authorization': 'key live_secret_key_68791341fdd94846a146f0457ff7b455',
+            'Content-Type': 'application/json',
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        new_response = json.loads(response.text)
         data = request.POST
         book = Booking.objects.create( professional=prof, customer=client,
-                                      book_date=data['date'], book_days=data['day'], book_hours=data['hour'])
+                                                book_date=data['date'], book_days=data['day'], book_hours=data['hour'])
         
-        # customer_email = request.user.email
-        # subject = 'Booking Confirmation from GharSewa'
-        # message = "Your booking is confirmed.\n Please view your booking details in the dashboard. \nIf you have any queries, call us or write to us via Contact Us Form. \n\nThank You!"
-        # from_email = 'agharsewa@gmail.com'
-        # recipient_list = [customer_email]
-        # send_mail(subject, message, from_email, recipient_list)
+        return redirect(new_response['payment_url'])
+    
+        
+
+        
 
     return render(request, 'booking.html', {'prof': prof, 'customer': client})
+            
+
+def verify(request):
+    url = "https://a.khalti.com/api/v2/epayment/lookup/"
+    pidx = request.GET.get('pidx')
+    headers = {
+            'Authorization': 'key live_secret_key_68791341fdd94846a146f0457ff7b455',
+            'Content-Type': 'application/json',
+        }
+    payload = json.dumps({
+        'pidx': pidx
+    })
+    response = requests.request("POST", url, headers=headers, data=payload)  
+    print(response.text)
+    new_response = json.loads(response.text)
+
+    if new_response['status'] =="Completed":  
+        customer_email = request.user.email
+        subject = 'Booking Confirmation from GharSewa'
+        message = "Your booking is confirmed.\n Please view your booking details in the dashboard. \nIf you have any queries, call us or write to us via Contact Us Form. \n\nThank You!"
+        from_email = 'agharsewa@gmail.com'
+        recipient_list = [customer_email]
+        print("customer_email: ", customer_email)
+        print("recipient_list: ", recipient_list)
+        send_mail(subject, message, from_email, recipient_list)
+
+
+    return redirect('/.')
     
-
-
 
 
 def contact(request):
@@ -210,5 +322,33 @@ def serviceView(request, myid):
     return render(request, 'service_view.html', context)
 
 
-def payment(request):
-    return render(request,'payment.html')
+
+@login_required(login_url='login')
+def booking_detail(request):
+    page = True
+    user = User.objects.get(id=request.user.id)
+    try:
+        customer = userRegister.objects.get(user=user)
+        books = Booking.objects.filter(customer=customer).order_by('-book_date')
+    except:
+        return redirect('user_booking')
+
+    context = {'books': books, 'page': page}
+    return render(request, 'bookingdetails.html', context)
+
+@login_required(login_url='login')
+def profBooking(request):
+    page = False
+    user = User.objects.get(id=request.user.id)
+
+    serv = profRegister.objects.get(user=user)
+    books = Booking.objects.filter(professional=serv)
+
+    context = {'books': books, 'page': page}
+    return render(request, 'bookingdetails.html', context)
+
+@login_required(login_url='login')
+def cancelBooking(request,cancel_id):
+    ser = Booking.objects.get(id=cancel_id)
+    ser.delete()
+    return redirect('bookingdetails')
